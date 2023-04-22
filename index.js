bb4cfec36120645a0872e90e1228e0fb8b8f1e4a
Victoria Merkiel <vlmerkiel@gmail.com>
2023-04-22 01:31:07 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Follow along with the examples here
+function doNothing() {}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+  function sayHelloToSamip() {
+    console.log("Hello, Samip!");
+  }
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  say("Goodbye", "Julio");
+  say("Julio", "hello");
+  function say(greeting, firstName) {
+    console.log("firstName: ", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+  }
+  function add(x, y) {
+    return x + y;
+  }
+  console.log(add(1, 2));
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+  }
+  console.log(say("Hello", "Liz"));
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log("I was called!");
+  }
+  console.log(say("Howdy", "partner"));
+  function say(greeting, firstName) {
+    console.log("I was called!");
+    return `${greeting}, ${firstName}!`;
+  }
